6399af9a3a28f8b864fb8a00456717278ae24b90
Alex Hauge <alexphauge@gmail.com>
2016-12-21 15:09:58 +0100

    Basic ui in place.

--- MatReqs/MainWindow.xaml.cs
+++ MatReqs/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace MatReqs
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<CalculatedItem> resultingItems = new List<CalculatedItem>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void btn_Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            resultingItems.Add(new CalculatedItem() { Title = "Test 1", Amount = 2, InInventory = false });
+            resultingItems.Add(new CalculatedItem() { Title = "Test 2", Amount = 4, InInventory = false });
+            resultingItems.Add(new CalculatedItem() { Title = "Test 3", Amount = 8, InInventory = false });
+
+            listBox_CalculatedResult.ItemsSource = resultingItems;
+        }
+        /* // Future implementation maybe (When clicking on a row, it should change the checkbox accordingly)
+        private void listBox_CalculatedResult_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(listBox_CalculatedResult.SelectedItem != null)
+            {
+                CalculatedItem selectedItem = (listBox_CalculatedResult.SelectedItem as CalculatedItem);
+
+                selectedItem.InInventory = selectedItem.InInventory ? false : true;
+
+                listBox_CalculatedResult.ItemsSource = resultingItems;
+            }
+        }*/
+    }
+
+    public class CalculatedItem
+    {
+        public string Title { get; set; }
+        public int Amount { get; set; }
+        public bool InInventory { get; set; }
     }
 }
